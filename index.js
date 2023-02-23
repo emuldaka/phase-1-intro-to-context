d1ef1c0235f6a01b949192bdccdb5198d236a543
Elis <emuldaka@gmail.com>
2023-02-22 23:22:17 -0600

    1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  const employeeRecord = {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employeeRecord;
+}
+
+function createEmployeeRecords(employees) {
+  let employeeData = [];
+  for (let i = 0; i < employees.length; i++) {
+    const employeeRecord = createEmployeeRecord(employees[i]);
+    employeeData.push(employeeRecord);
+  }
+  return employeeData;
+}
+// timestamps:"2018-01-01 2300"
+function createTimeInEvent(employeeData, dateStamp) {
+  const date = [];
+  const time = [];
+  for (let i = 0; i < dateStamp.length; i++) {
+    if (i < 10) {
+      date.push(dateStamp[i]);
+    } else if (i > 10) {
+      time.push(dateStamp[i]);
+    }
+  }
+  employeeData.timeInEvents.push({
+    type: "TimeIn",
+    hour: time.join(""),
+    date: date.join(""),
+  });
+  return employeeData;
+}
